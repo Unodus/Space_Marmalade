048389b716ab8a657a11c215f4de4749d5b3ec68
James Bradbury <unkoun98@gmail.com>
2021-04-13 11:34:26 +0100

    push latest elias fixes

---
+++ Assets/Scripts/Sound/ScriptableElias.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EliasProfile", menuName = "ScriptableObjects/EliasProfile", order = 1)]
+
+public class ScriptableElias : ScriptableObject
+{
+
+    public enum Themes // All registered elias themes 
+    {
+        Strategy,
+        Combat,
+        Combat2
+
+    };
+
+    [Serializable]
+    public class EliasPalettes // Each palette
+    {
+        public string EliasName;
+        public Themes MusicType;
+     
+    }
+
+    public EliasPalettes[] eliasPalette;    // array of all palettes
+
+}
