1e0d3762625bf33f7c717b565d06ec0d5d51109c
James Bradbury <unkoun98@gmail.com>
2021-07-13 23:29:26 +0100

    Made boot scene actually boot game

--- Assets/Scripts/Data Handlers/Boot.cs
+++ Assets/Scripts/Data Handlers/Boot.cs
@@ -1,27 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Boot : MonoBehaviour
 {
 
     [SerializeField]  DialogueRecorder ConsoleLog;
+
+    [SerializeField] ScriptableScenes ScenesToLoadOnBoot;
+
     List<IEnumerator> StartUpProcesses;
     WaitForEndOfFrame RestFrame;
 
+    [SerializeField] bool GameLoaded;
+
     void Start()
     {
-        
+        StartBoot();
+    }
+
+
+    public void StartBoot()
+    {
+        LogToConsole("Boot Started");
         StartUpProcesses = new List<IEnumerator>();
         #region
         // Put Startup Enumerators here:
 
         StartUpProcesses.Add(InitialiseBootVariables());
-        
+
         // ^^^^^
         #endregion
         StartCoroutine(Startup(StartUpProcesses.ToArray()));
+        LogToConsole("Ready");
     }
+
+    public void LoadBuild()
+    {
+        LogToConsole("Loading Game");
+        StartUpProcesses = new List<IEnumerator>();
+        #region
+        // Put Build Enumerators here:
+
+        StartUpProcesses.Add(LoadBootScenes());
+
+
+        // ^^^^^
+        #endregion
+        StartCoroutine(Startup(StartUpProcesses.ToArray()));
+
+
+    }
+
+    public void UnloadBuild()
+    {
+        LogToConsole("Unloading Game");
+        StartUpProcesses = new List<IEnumerator>();
+        #region
+        // Put Build Enumerators here:
+
+        StartUpProcesses.Add(UnloadBootScenes());
+
+
+        // ^^^^^
+        #endregion
+        StartCoroutine(Startup(StartUpProcesses.ToArray()));
+ 
+    }
+
+
 
     public void LogToConsole(string LogThis)
     {
@@ -31,20 +79,69 @@ public class Boot : MonoBehaviour
 
     IEnumerator Startup(IEnumerator[] StartUpProcesses)
     {
-        LogToConsole("Boot Started");
+
         yield return RestFrame;
         foreach (IEnumerator i in StartUpProcesses)
         {
             StartCoroutine(i);
             yield return RestFrame;
         }
-        LogToConsole("Ready");
     }
+
+
+
+
 
     IEnumerator InitialiseBootVariables()
     {
         RestFrame = new WaitForEndOfFrame();
+
+        GameLoaded = false;
+
         LogToConsole("Boot Loaded");
         yield return null;
+    }
+    
+    IEnumerator LoadBootScenes()
+    {
+        if (!GameLoaded) LogToConsole("Completed Load");
+        else LogToConsole("Load Failed");
+
+
+        if (ScenesToLoadOnBoot == null || GameLoaded) yield return null;
+
+        else
+        {
+
+            foreach (ScriptableScenes.SceneType i in ScenesToLoadOnBoot.ScenesToLoadOnBoot)
+            {
+                if (i.SceneName == null) continue;
+                SceneManager.LoadScene(i.SceneName, LoadSceneMode.Additive);
+            }
+
+            GameLoaded = true;
+            yield return null;
+
+
+        }
+    }
+    
+    IEnumerator UnloadBootScenes()
+    {
+        if (GameLoaded) LogToConsole("Completed Load");
+        else LogToConsole("Load Failed");
+
+        if (ScenesToLoadOnBoot == null || !GameLoaded) yield return null;
+        else
+        {
+            foreach (ScriptableScenes.SceneType i in ScenesToLoadOnBoot.ScenesToLoadOnBoot)
+            {
+                if (i.SceneName == null) continue;
+                SceneManager.UnloadSceneAsync(i.SceneName);
+            }
+            GameLoaded = false;
+            yield return null;
+
+        }
     }
 }
